3f73b1948f9dce245104904e74398b6128c90d38
jchlopak <jdrzyzga10@gmail.com>
2024-10-13 13:01:29 +0200

    FIX

--- Lab 2/pythonProject/main.py
+++ Lab 2/pythonProject/main.py
@@ -1,19 +1,4 @@
-import os
-import importlib
-
 import tasks.t_2_1
-
-# Path to the folder
-folder_path = 'tasks'
-
-# Loop through all files in the folder
-for filename in os.listdir(folder_path):
-    if filename.endswith('.py') and filename != '__init__.py':
-        # Remove the .py extension to get the module name
-        module_name = filename[:-3]
-
-        # Import the module
-        importlib.import_module(f"{folder_path}.{module_name}")
 
 if __name__ == '__main__':
     user_input = input("Enter a number (1-16) to choose an option: ")
